6a7a4a8041b6edef482be691a46c4d02b6c1ec55
Khomotso <khometja@gmail.com>
2023-02-19 11:18:51 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point.
+ *
+ * function main - Prints integers 0 to 9.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int jod;
+
+	for (jod = 0; jod < 10; jod++)
+	{
+		printf("%i", jod);
+	}
+	printf("\n");
+	return (0);
+}
