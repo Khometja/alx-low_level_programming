50c7d158c2f24d780ee67afccf1cb36b343dec19
Khomotso <khometja@gmail.com>
2023-02-21 00:51:13 -0800

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print abc * 10
+ *
+ * Return: Nothing.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char b, m;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
