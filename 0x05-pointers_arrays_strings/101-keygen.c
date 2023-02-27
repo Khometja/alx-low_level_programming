ae37a17848e19fb7269d1d9290d0708eb358d2e3
Khomotso <khometja@gmail.com>
2023-02-27 11:49:47 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,3 @@
-BrightDaniel
-/
-alx-low_level_programming
-Public
-Code
-Issues
-7
-Pull requests
-1
-Actions
-Projects
-Security
-Insights
-Beta Try the new code view
-alx-low_level_programming/0x05-pointers_arrays_strings/101-keygen.c
-@BrightDaniel
-BrightDaniel Optional task 2
- 1 contributor
-54 lines (46 sloc)  881 Bytes
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
